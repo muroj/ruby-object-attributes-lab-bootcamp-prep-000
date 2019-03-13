bf4dacfebb2cb6c81b47ba33b896942bb80d36d9
Joe Muro <jmuro0321@gmail.com>
2019-03-13 16:58:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,5 +3,15 @@ class Dog
     @name
   end
   
-  def name=()
+  def name=(new_name)
+    @name = new_name
   end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+end
